d2e382a16f147e4584257625f180557d29f51536
Sandra Dery-Kuuzume <123436834+akuuzume@users.noreply.github.com>
2025-09-07 10:59:24 +0000

    Update bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,21 +1,19 @@
-class BankAccount():
+class BankAccount:
     def __init__(self, initial_balance=0):
         self.account_balance = initial_balance
-    
 
     def deposit(self, amount):
-        if amount>0:
-              self.account_balance +=amount
+        if amount > 0:
+            self.account_balance += amount
         else:
-              print("Insufficient balance to deposit")
-        
+            print("Cannot deposit a non-positive amount.")
 
     def withdraw(self, amount):
-        if amount>0 and self.account_balance >=amount:
-            self.account_balance -=amount
+        if amount > 0 and self.account_balance >= amount:
+            self.account_balance -= amount
             return True
         else:
             return False
-            
-    def displace_balance(self):
-         print(f"The balance is {self.account_balance}.3f")
+
+    def display_balance(self):
+        print(f"The balance is {self.account_balance:.3f}")
