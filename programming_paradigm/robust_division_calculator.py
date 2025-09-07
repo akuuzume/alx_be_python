d346bc1bbdffca4821ec1dbda940cc46cb6654cc
Sandra Dery-Kuuzume <123436834+akuuzume@users.noreply.github.com>
2025-09-07 11:33:41 +0000

    Update robust_division_calculator.py

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,9 +1,11 @@
+# robust_division_calculator.py
+
 def safe_divide(numerator, denominator):
     try:
         numerator = float(numerator)
         denominator = float(denominator)
     except ValueError:
-        return "Error: Invalid input. Please enter numbers only."
+        return "Error: Please enter numeric values only."
 
     try:
         return numerator / denominator
